1f154cc45f68f02fb46a5f7b3174b6c1676cc871
Jest.privi <jest.privi@gmail.com>
2012-01-14 08:19:50 +0000

    First commit.

---
+++ 2p-android/branches/TB-privitera/ProgettoTesi/src/gianluca/tesi/SplashScreen.java
@@ -0,0 +1,47 @@
+package gianluca.tesi;
+
+import android.app.Activity;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+	private final int SPLASH_DISPLAY_LENGHT = 1000;
+
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.splashscreen);
+
+		/*
+		 * New Handler to start the Menu-Activity
+		 * 
+		 * and close this Splash-Screen after some seconds.
+		 */
+
+		new Handler().postDelayed(new Runnable() {
+
+			public void run() {
+
+				/* Create an Intent that will start the Menu-Activity. */
+
+				Intent mainIntent = new Intent(SplashScreen.this,
+						tuPrologActivity.class);
+
+				SplashScreen.this.startActivity(mainIntent);
+
+				SplashScreen.this.finish();
+
+			}
+
+		}, SPLASH_DISPLAY_LENGHT);
+
+	}
+
+}
---
+++ 2p-android/branches/TB-privitera/ProgettoTesi/src/gianluca/tesi/TheoryEditActivity.java
@@ -0,0 +1,96 @@
+package gianluca.tesi;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class TheoryEditActivity extends Activity {
+	
+	private EditText mTitleText;
+	private EditText mBodyText;
+	private Long mRowId;
+	private TheoryDbAdapter mDbHelper;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		mDbHelper = new TheoryDbAdapter(this);
+		mDbHelper.open();
+
+		setContentView(R.layout.theory_edit);
+
+		mTitleText = (EditText) findViewById(R.id.title);
+		mBodyText = (EditText) findViewById(R.id.body);
+
+		Button confirmButton = (Button) findViewById(R.id.confirm);
+
+		mRowId = (savedInstanceState == null) ? null
+				: (Long) savedInstanceState
+						.getSerializable(TheoryDbAdapter.KEY_ROWID);
+		if (mRowId == null) {
+			Bundle extras = getIntent().getExtras();
+			mRowId = extras != null ? extras.getLong(TheoryDbAdapter.KEY_ROWID)
+					: null;
+		}
+
+		populateFields();
+
+		confirmButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				setResult(RESULT_OK);
+				finish();
+			}
+
+		});
+	}
+
+	private void populateFields() {
+		if (mRowId != null) {
+			Cursor theory = mDbHelper.fetchTheory(mRowId);
+			startManagingCursor(theory);
+			mTitleText.setText(theory.getString(theory
+					.getColumnIndexOrThrow(TheoryDbAdapter.KEY_TITLE)));
+			mBodyText.setText(theory.getString(theory
+					.getColumnIndexOrThrow(TheoryDbAdapter.KEY_BODY)));
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		saveState();
+		outState.putSerializable(TheoryDbAdapter.KEY_ROWID, mRowId);
+	}
+	
+	@Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
+	
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateFields();
+    }
+    
+    private void saveState() {
+        String title = mTitleText.getText().toString();
+        String body = mBodyText.getText().toString();
+
+        if (mRowId == null) {
+            long id = mDbHelper.createTheory(title, body);
+            if (id > 0) {
+                mRowId = id;
+            }
+        } else {
+            mDbHelper.updateTheory(mRowId, title, body);
+        }
+    }
+
+}
