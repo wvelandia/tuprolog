af659629085a5079db3a01656cdccfd440fc6af0
Jest.privi <jest.privi@gmail.com>
2012-01-14 10:34:15 +0000

    Renamed packages.

--- 2p-android/branches/TB-privitera/src/alice/tuprologx/android/CUIConsole.java
+++ 2p-android/branches/TB-privitera/src/alice/tuprologx/android/CUIConsole.java
@@ -1,4 +1,4 @@
-package gianluca.tesi;
+package alice.tuprologx.android;
 
 import java.io.Serializable;
 import java.util.ArrayList;
--- 2p-android/branches/TB-privitera/src/alice/tuprologx/android/TheoryEditActivity.java
+++ 2p-android/branches/TB-privitera/src/alice/tuprologx/android/TheoryEditActivity.java
@@ -1,4 +1,4 @@
-package gianluca.tesi;
+package alice.tuprologx.android;
 
 import android.app.Activity;
 import android.database.Cursor;
