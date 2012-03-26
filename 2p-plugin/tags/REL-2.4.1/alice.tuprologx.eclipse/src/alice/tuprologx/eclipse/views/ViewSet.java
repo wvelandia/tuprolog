5437c0c33b10c40d67b22b3c74bcc9c16d866f5c
alemonty88 <AleMonty88@gmail.com>
2012-03-26 09:13:50 +0000

    fixed build error after creation of a project

--- 2p-plugin/tags/REL-2.4.1/alice.tuprologx.eclipse/src/alice/tuprologx/eclipse/views/ViewSet.java
+++ 2p-plugin/tags/REL-2.4.1/alice.tuprologx.eclipse/src/alice/tuprologx/eclipse/views/ViewSet.java
@@ -7,7 +7,9 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 import alice.tuprologx.eclipse.core.PrologQuery;
@@ -17,39 +19,49 @@ public class ViewSet implements Observer {
 
 	private ConsoleView console;
 	private QueryList queryList;
-//	private ASTView ASTview;
+	//	private ASTView ASTview;
 	public ViewSet() {
 		PrologQueryFactory.getInstance().addObserver(this);
 	}
 
 	public void refresh(final String theoryToShow) {
-		final IWorkbenchPage wp = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow().getActivePage();
-		try {
-			IViewReference[] viewList = wp.getViewReferences();
-			for(IViewReference ref : viewList){
-				if(ref.getId().equalsIgnoreCase("alice.tuprologx.eclipse.views.ConsoleView")){
-					console = (ConsoleView) ref.getView(false);
-				}
-				if(ref.getId().equalsIgnoreCase("alice.tuprologx.eclipse.views.QueryList")){
-					queryList = (QueryList) ref.getView(false);
-				}
-			}
+		//IWorkbench workbench = PlatformUI.getWorkbench();
+		//IWorkbenchWindow activeWindow = workbench.getActiveWorkbenchWindow();
+		//IWorkbenchPage activePage = activeWindow.getActivePage();
 
-			queryList.addSelectionListener(new SelectionListener(){
-				public void widgetSelected(SelectionEvent e) {
-					TreeItem selection = (TreeItem)e.item;
-					PrologQuery query = (PrologQuery) selection.getData();
-					console.setQuery(query);
-					wp.activate(console);
-				}
-				public void widgetDefaultSelected(SelectionEvent e) {
-				}
-			});
 
-			//} catch (PartInitException e) {
-		} catch (NullPointerException e) {
-		}
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() { 
+			@Override
+			public void run() { 
+				IWorkbenchWindow dwindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow(); 
+				final IWorkbenchPage wp = dwindow.getActivePage(); 
+				//final IWorkbenchPage wp = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				try {
+					IViewReference[] viewList = wp.getViewReferences();
+					for(IViewReference ref : viewList){
+						if(ref.getId().equalsIgnoreCase("alice.tuprologx.eclipse.views.ConsoleView")){
+							console = (ConsoleView) ref.getView(false);
+						}
+						if(ref.getId().equalsIgnoreCase("alice.tuprologx.eclipse.views.QueryList")){
+							queryList = (QueryList) ref.getView(false);
+						}
+					}
+
+					queryList.addSelectionListener(new SelectionListener(){
+						public void widgetSelected(SelectionEvent e) {
+							TreeItem selection = (TreeItem)e.item;
+							PrologQuery query = (PrologQuery) selection.getData();
+							console.setQuery(query);
+							wp.activate(console);
+						}
+						public void widgetDefaultSelected(SelectionEvent e) {
+						}
+					});
+
+					//} catch (PartInitException e) {
+				} catch (NullPointerException e) {
+				}
+			}});
 	}
 
 	public void update(Observable arg0, Object arg1){
