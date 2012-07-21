ef9b78adc11233fa75ea296df80e5328c10ab7df
alemonty88 <AleMonty88@gmail.com>
2012-07-21 12:53:59 +0000

    Reintegrated TB-Privitera branch

--- 2p-android/trunk/src/alice/tuprologx/android/TheoryEditActivity.java
+++ 2p-android/trunk/src/alice/tuprologx/android/TheoryEditActivity.java
@@ -9,92 +9,90 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class TheoryEditActivity extends Activity {
-	
-	private EditText mTitleText;
-	private EditText mBodyText;
-	private Long mRowId;
-	private TheoryDbAdapter mDbHelper;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
 
-		mDbHelper = new TheoryDbAdapter(this);
-		mDbHelper.open();
+  private EditText mTitleText;
+  private EditText mBodyText;
+  private Long mRowId;
+  private TheoryDbAdapter mDbHelper;
 
-		setContentView(R.layout.theory_edit);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-		mTitleText = (EditText) findViewById(R.id.title);
-		mBodyText = (EditText) findViewById(R.id.body);
+    mDbHelper = new TheoryDbAdapter(this);
+    mDbHelper.open();
 
-		Button confirmButton = (Button) findViewById(R.id.confirm);
+    setContentView(R.layout.theory_edit);
 
-		mRowId = (savedInstanceState == null) ? null
-				: (Long) savedInstanceState
-						.getSerializable(TheoryDbAdapter.KEY_ROWID);
-		if (mRowId == null) {
-			Bundle extras = getIntent().getExtras();
-			mRowId = extras != null ? extras.getLong(TheoryDbAdapter.KEY_ROWID)
-					: null;
-		}
+    mTitleText = (EditText) findViewById(R.id.title);
+    mBodyText = (EditText) findViewById(R.id.body);
 
-		populateFields();
+    Button confirmButton = (Button) findViewById(R.id.confirm);
 
-		confirmButton.setOnClickListener(new View.OnClickListener() {
-
-			public void onClick(View view) {
-				setResult(RESULT_OK);
-				saveState();
-				mDbHelper.close();
-				finish();
-			}
-
-		});
-	}
-
-	private void populateFields() {
-		if (mRowId != null) {
-			Cursor theory = mDbHelper.fetchTheory(mRowId);
-			startManagingCursor(theory);
-			mTitleText.setText(theory.getString(theory
-					.getColumnIndexOrThrow(TheoryDbAdapter.KEY_TITLE)));
-			mBodyText.setText(theory.getString(theory
-					.getColumnIndexOrThrow(TheoryDbAdapter.KEY_BODY)));
-		}
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-//		saveState();
-		outState.putSerializable(TheoryDbAdapter.KEY_ROWID, mRowId);
-	}
-	
-	@Override
-    protected void onPause() {
-        super.onPause();
-    }
-	
-    @Override
-    protected void onResume() {
-        super.onResume();
-        populateFields();
-    }
-    
-    private void saveState() {
-        String title = mTitleText.getText().toString();
-        String body = mBodyText.getText().toString();
-
-        if (mRowId == null && !title.equals("")) {
-            long id = mDbHelper.createTheory(title, body);
-            if (id > 0) {
-                mRowId = id;
-            }
-        } else if (mRowId != null) {
-            mDbHelper.updateTheory(mRowId, title, body);
-        } else if (title.equals("")) {
-			Toast.makeText(getApplicationContext(), "Invalid Title", Toast.LENGTH_SHORT).show();
-        }
+    mRowId = (savedInstanceState == null) ? null : (Long) savedInstanceState
+        .getSerializable(TheoryDbAdapter.KEY_ROWID);
+    if (mRowId == null) {
+      Bundle extras = getIntent().getExtras();
+      mRowId = extras != null ? extras.getLong(TheoryDbAdapter.KEY_ROWID)
+          : null;
     }
 
+    populateFields();
+
+    confirmButton.setOnClickListener(new View.OnClickListener() {
+
+      public void onClick(View view) {
+        setResult(RESULT_OK);
+        saveState();
+        mDbHelper.close();
+        finish();
+      }
+
+    });
+  }
+
+  private void populateFields() {
+    if (mRowId != null) {
+      Cursor theory = mDbHelper.fetchTheory(mRowId);
+      startManagingCursor(theory);
+      mTitleText.setText(theory.getString(theory
+          .getColumnIndexOrThrow(TheoryDbAdapter.KEY_TITLE)));
+      mBodyText.setText(theory.getString(theory
+          .getColumnIndexOrThrow(TheoryDbAdapter.KEY_BODY)));
+    }
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putSerializable(TheoryDbAdapter.KEY_ROWID, mRowId);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    populateFields();
+  }
+
+  private void saveState() {
+    String title = mTitleText.getText().toString();
+    String body = mBodyText.getText().toString();
+
+    if (mRowId == null && !title.equals("")) {
+      long id = mDbHelper.createTheory(title, body);
+      if (id > 0) {
+        mRowId = id;
+      }
+    } else if (mRowId != null) {
+      mDbHelper.updateTheory(mRowId, title, body);
+    } else if (title.equals("")) {
+      Toast.makeText(getApplicationContext(), "Invalid Title",
+          Toast.LENGTH_SHORT).show();
+    }
+  }
 }
--- 2p-android/trunk/src/alice/tuprologx/android/TheoryImportActivity.java
+++ 2p-android/trunk/src/alice/tuprologx/android/TheoryImportActivity.java
@@ -16,135 +16,127 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class TheoryImportActivity extends ListActivity {
-	
-	private final static String MY_PREFERENCES = "MyPref";
-    private final static String PATH_DATA_KEY = "lastData";
 
-	private enum DISPLAYMODE {
-		ABSOLUTE, RELATIVE;
-	}
+  private final static String MY_PREFERENCES = "MyPref";
+  private final static String PATH_DATA_KEY = "lastData";
 
-	private final DISPLAYMODE displayMode = DISPLAYMODE.ABSOLUTE;
-	private List<String> directoryEntries = new ArrayList<String>();
-	private File currentDirectory = new File("/");
+  private enum DISPLAYMODE {
+    ABSOLUTE, RELATIVE;
+  }
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
-		// setContentView() gets called within the next line,
-		// so we do not need it here.
-		browseToPref();
-	}
+  private final DISPLAYMODE displayMode = DISPLAYMODE.ABSOLUTE;
+  private List<String> directoryEntries = new ArrayList<String>();
+  private File currentDirectory = new File("/");
 
-	/**
-	 * This function browses to the root-directory of the file-system.
-	 */
-	private void browseToPref() {
-		SharedPreferences prefs = getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
-	    String s = prefs.getString(PATH_DATA_KEY, "/");
-		browseTo(new File(s));
-	}
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    browseToPref();
+  }
 
-	private void upOneLevel() {
-		if (this.currentDirectory.getParent() != null)
-			this.browseTo(this.currentDirectory.getParentFile());
-	}
+  private void browseToPref() {
+    SharedPreferences prefs = getSharedPreferences(MY_PREFERENCES,
+        Context.MODE_PRIVATE);
+    String s = prefs.getString(PATH_DATA_KEY, "/");
+    browseTo(new File(s));
+  }
 
-	private void browseTo(final File aDirectory) {
-		if (aDirectory.isDirectory()) {
-			this.currentDirectory = aDirectory;
-			fill(aDirectory.listFiles());
-		} else {
-			// Lets start an intent to View the file, that was
-			// clicked...
-			Bundle bundle = new Bundle();
-			String st = aDirectory.getAbsolutePath();
-			
-			saveLastData(aDirectory.getParent());
-			
-			bundle.putString("nomeFile", st);
-			Intent mIntent = new Intent();
-			mIntent.putExtras(bundle);
-			setResult(RESULT_OK, mIntent);
-			finish();
-		}
-	}
+  private void upOneLevel() {
+    if (this.currentDirectory.getParent() != null)
+      this.browseTo(this.currentDirectory.getParentFile());
+  }
 
-	private void fill(File[] files) {
-		this.directoryEntries.clear();
+  private void browseTo(final File aDirectory) {
+    if (aDirectory.isDirectory()) {
+      this.currentDirectory = aDirectory;
+      fill(aDirectory.listFiles());
+    } else {
+      Bundle bundle = new Bundle();
+      String st = aDirectory.getAbsolutePath();
 
-		// Add the "." and the ".." == 'Up one level'
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		}
-		this.directoryEntries.add(".");
+      saveLastData(aDirectory.getParent());
 
-		if (this.currentDirectory.getParent() != null)
-			this.directoryEntries.add("..");
+      bundle.putString("nomeFile", st);
+      Intent mIntent = new Intent();
+      mIntent.putExtras(bundle);
+      setResult(RESULT_OK, mIntent);
+      finish();
+    }
+  }
 
-		switch (this.displayMode) {
-		case ABSOLUTE:
-			for (File file : files) {
-				if (file.canRead()) {
-					if (file.getAbsolutePath().endsWith(".pl") || file.getAbsolutePath().endsWith(".txt")
-							|| file.getAbsolutePath().endsWith(".doc") || file.isDirectory()) {
-						this.directoryEntries.add(file.getPath());
-					}
-					
-				}
-			}
-			break;
-		case RELATIVE: // On relative Mode, we have to add the current-path to
-						// the beginning
-			int currentPathStringLenght = this.currentDirectory
-					.getAbsolutePath().length();
-			for (File file : files) {
-				this.directoryEntries.add(file.getAbsolutePath().substring(
-						currentPathStringLenght));
-			}
-			break;
-		}
+  private void fill(File[] files) {
+    this.directoryEntries.clear();
 
-		ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this,
-				R.layout.file_row, this.directoryEntries);
+    try {
+      Thread.sleep(10);
+    } catch (InterruptedException e1) {
+      e1.printStackTrace();
+    }
+    this.directoryEntries.add(".");
 
-		this.setListAdapter(directoryList);
-	}
+    if (this.currentDirectory.getParent() != null)
+      this.directoryEntries.add("..");
 
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-		//int selectionRowID = (int) this.getSelectedItemId();
-		String selectedFileString = this.directoryEntries.get(position);
-		if (selectedFileString.equals(".")) {
-			// Refresh
-			this.browseTo(this.currentDirectory);
-		} else if (selectedFileString.equals("..")) {
-			this.upOneLevel();
-		} else {
-			File clickedFile = null;
-			switch (this.displayMode) {
-			case RELATIVE:
-				clickedFile = new File(this.currentDirectory.getAbsolutePath()+ this.directoryEntries.get(position));
-				break;
-			case ABSOLUTE:
-				clickedFile = new File(this.directoryEntries.get(position));
-				break;
-			}
-			if (clickedFile != null)
-				this.browseTo(clickedFile);
-		}
-	}
-	
-	private void saveLastData(String s) {
-        SharedPreferences prefs = getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        if (s != null) {
-                editor.putString(PATH_DATA_KEY, s);
-                editor.commit();
+    switch (this.displayMode) {
+    case ABSOLUTE:
+      for (File file : files) {
+        if (file.canRead()) {
+          if (file.getAbsolutePath().endsWith(".pl")
+              || file.getAbsolutePath().endsWith(".txt")
+              || file.getAbsolutePath().endsWith(".doc") || file.isDirectory()) {
+            this.directoryEntries.add(file.getPath());
+          }
+
         }
-	}
+      }
+      break;
+    case RELATIVE:
+      int currentPathStringLenght = this.currentDirectory.getAbsolutePath()
+          .length();
+      for (File file : files) {
+        this.directoryEntries.add(file.getAbsolutePath().substring(
+            currentPathStringLenght));
+      }
+      break;
+    }
+
+    ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this,
+        R.layout.file_row, this.directoryEntries);
+
+    this.setListAdapter(directoryList);
+  }
+
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    super.onListItemClick(l, v, position, id);
+    String selectedFileString = this.directoryEntries.get(position);
+    if (selectedFileString.equals(".")) {
+      this.browseTo(this.currentDirectory);
+    } else if (selectedFileString.equals("..")) {
+      this.upOneLevel();
+    } else {
+      File clickedFile = null;
+      switch (this.displayMode) {
+      case RELATIVE:
+        clickedFile = new File(this.currentDirectory.getAbsolutePath()
+            + this.directoryEntries.get(position));
+        break;
+      case ABSOLUTE:
+        clickedFile = new File(this.directoryEntries.get(position));
+        break;
+      }
+      if (clickedFile != null)
+        this.browseTo(clickedFile);
+    }
+  }
+
+  private void saveLastData(String s) {
+    SharedPreferences prefs = getSharedPreferences(MY_PREFERENCES,
+        Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = prefs.edit();
+    if (s != null) {
+      editor.putString(PATH_DATA_KEY, s);
+      editor.commit();
+    }
+  }
 }
