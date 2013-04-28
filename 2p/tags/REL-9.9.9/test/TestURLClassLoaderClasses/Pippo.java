cff682c60a093a23af246d9886e034a37575b742
alemonty88@gmail.com <alemonty88@gmail.com@1341b3fe-529b-92d5-dec5-adfe41881907>
2013-04-28 10:41:23 +0000

    TEST

---
+++ 2p/tags/REL-9.9.9/test/TestURLClassLoaderClasses/Pippo.java
@@ -0,0 +1,12 @@
+package TestURLClassLoaderClasses;
+public class Pippo implements IPippo {
+	
+	public Pippo(){ 
+		System.out.println("Pippo constructed!"); 
+	}
+	
+	public void met() 
+	{
+		System.out.println("met"); 
+	}
+}
