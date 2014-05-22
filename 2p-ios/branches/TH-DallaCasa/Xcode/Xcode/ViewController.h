94fce1df6d7fdabba0828d68b48b8bf51de36e8d
lorenzo.dallacasa <lorenzo.dallacasa@gmail.com>
2014-05-22 03:59:27 +0000

    First version of tuprolog app for ios. Simple UI created with Xcode and then integrated in Eclipse. Ability to fire and catch events but impossibility to use same class variables.

---
+++ 2p-ios/branches/TH-DallaCasa/Xcode/Xcode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Xcode
+//
+//  Created by Lorenzo Dalla Casa on 22/05/14.
+//  Copyright (c) 2014 Lorenzo Dalla Casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
